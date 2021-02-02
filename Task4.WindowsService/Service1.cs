fe5ace3bc58c43c0ababd040cd5edb4d1bab4a2b
ivan_gorchichko <ivangorchichko@mail.ru>
2021-02-02 23:23:01 +0300

    Windows service changes

--- Task4.WindowsService/Service1.cs
+++ Task4.WindowsService/Service1.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Configuration;
-using System.Data;
-using System.Diagnostics;
+﻿using System.Configuration;
 using System.IO;
-using System.Linq;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
 using Task4.BL.Contracts;
 using Task4.BL.Service;
 
